ef31928c640f73b945ad979c4e6cf20a11989b7a
nejcmlakar12 <nejcmlakar11@gmail.com>
2024-09-24 10:09:26 +0200

    Update

--- Dashboard.php
+++ Dashboard.php
@@ -40,7 +40,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Dashboard
+    </title>
     <link rel="stylesheet" href="stil.css">
 </head>
 <body class="background">
