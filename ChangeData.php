c75cce50372e47b5625a195bdc56cfd96e360c2f
bedakcool <153218579+raziskovalnaleon@users.noreply.github.com>
2024-10-22 10:49:50 +0200

    check.php file updated, added changedata.php

--- ChangeData.php
+++ ChangeData.php
@@ -20,46 +20,17 @@ if (mysqli_num_rows($result) > 0){
        $BasePassword = $row["password"];
    }
 }
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if(isset($_POST['check_pass'])){
-        $password = $_POST["pass"];
-
-        if(password_verify($password, $BasePassword)){
-            header("location:ChangeData.php");
-            exit();
-        }
-        else{
-            $error = "Wrong Password!";
-        }
-    }
-}
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
 <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Check
+    <title>Change Data
     </title>
     <link rel="stylesheet" href="stil.css">
 </head>
 <body>
-    <div class="check-box">
-        <form method="post" style="display:inline;">
-            <div class="check-main">
-                <input type="password" placeholder="Enter Password" name="pass" required>
-                <button type="submit" name="check_pass" class="check-button">
-                    Check
-                </button>
-            </div>
-        </form>
-    </div>
-    <div class="error-type">
-        <?php 
-            if(isset($error)){
-                echo $error;
-            }
-        ?>
-    </div>
+    
 </body>
 </html>
