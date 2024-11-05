225811ee727cac1848863b4786cdde09ca30e0c4
Nejc Mlakar <117648642+nejcmlakar12@users.noreply.github.com>
2024-11-05 09:42:41 +0100

    update

--- oddajaNaloge.php
+++ oddajaNaloge.php
@@ -6,7 +6,7 @@ $dbname = "smw";
 session_start();
 
 $filename = "";
-$filepath = 'uploads/user/';
+$filepath = '/var/www/html/SMW4/uploads/users';
 
 if (!isset($_SESSION["uname"]) || !isset($_SESSION["pass"])) {
     header("location:Registration.php");
